6dbfa5a038b16fb4350ab980baeeb1e0bc7f5c2e
joschonarth <joschonarth@gmail.com>
2024-12-15 17:41:26 -0300

    feat: register user in the database

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,4 +1,6 @@
+import { AppError } from "@/utils/app-error";
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
 import { hash } from "bcrypt";
 import { z } from "zod"
 
@@ -12,9 +14,25 @@ class UsersController {
 
         const { name, email, password } = bodySchema.parse(request.body)
 
+        const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+        if (userWithSameEmail) {
+            throw new AppError("User with same email already exists")
+        }
+
         const hashedPassword = await hash(password, 8)
 
-        return response.json({ message: "User created successfully" })
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+
+        const { password: _, ...userWithoutPassword } = user
+
+        return response.json(userWithoutPassword)
     }
 }
 
