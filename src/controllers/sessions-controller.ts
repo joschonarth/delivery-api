a2d9a2c25d168107be532d8f4a443a5b1cdbc3a6
joschonarth <joschonarth@gmail.com>
2024-12-15 18:16:41 -0300

    feat: create authentication controller and route

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,34 @@
+import { AppError } from "@/utils/app-error";
+import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod"
+
+class SessionsController {
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const { email, password } = bodySchema.parse(request.body)
+
+        const user = await prisma.user.findFirst({
+            where: { email }
+        })
+
+        if (!user) {
+            throw new AppError("User not found", 404)
+        }
+
+        const passwordMatched = await compare(password, user.password)
+
+        if (!passwordMatched) {
+            throw new AppError("Invalid credentials", 401)
+        }
+
+        return response.json({ message: "Ok" })
+    }
+}
+
+export { SessionsController }
