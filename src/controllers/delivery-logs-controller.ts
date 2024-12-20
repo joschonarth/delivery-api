7a937d31f1f4e2bd850f1c37be82c25444aaea5f
joschonarth <joschonarth@gmail.com>
2024-12-20 18:57:32 -0300

    fix: show delivery not foud case not exist

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -53,6 +53,10 @@ class DeliveryLogsController {
             }
         })
 
+        if (!delivery) {
+            return response.status(404).json({ message: "Delivery not found" })
+        }
+
         if (request.user?.role === "customer" && request.user.id !== delivery?.userId) {
             throw new AppError("The user can only view their own deliveries", 401)
         }
